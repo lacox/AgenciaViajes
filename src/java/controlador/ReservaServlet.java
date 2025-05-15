a449fdb6d0b3cdf99753d63e3061cb7d95fae0d9
lacox <lacox@Neon>
2025-05-14 19:48:31 -0600

    Clase dedicada a devolver una lista de objetos de todos los vuelos encontrados en la base de datos con el origen y destino solicitados.
    Y comienzo del servlet dedicado a obtener los datos y reenviarlos a un
    jsp.

---
+++ src/java/controlador/ReservaServlet.java
@@ -0,0 +1,30 @@
+package controlador;
+import objects.Vuelo;
+import objects.VueloDAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Angel David Salas Mendoza
+ */
+
+public class ReservaServlet extends HttpServlet {
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String
+    }
+}
